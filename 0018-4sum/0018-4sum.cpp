f916f5ada5ce0496b783d1bbcc8f39c1d55526e9
Yash-C1 <56033766+Yash-C1@users.noreply.github.com>
2025-10-07 17:04:32 -0700

    Time: 23 ms (52.43%), Space: 17.4 MB (83.02%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -31,35 +31,5 @@ public:
         }
         
         return ans;
-        // vector<vector<int>> ans;
-        // sort(nums.begin(),nums.end());
-        // if(nums.size()<4) return {};
-        // for(int i = 0; i< nums.size()-3;i++){
-        //     if(i!=0 && nums[i]==nums[i-1]) continue;
-        //     for(int j = i+1; j<nums.size()-2;j++){
-        //         if(nums.size()>1 && j!=i+1 && nums[j]==nums[j-1]) continue;
-                
-        //         int a = j+1;
-        //         int b = nums.size()-1;
-        //         while(a<b){
-        //             long long sum = nums[i]+nums[j];
-        //             sum+=nums[a];
-        //             sum+=nums[b];
-        //             if(sum==target){
-        //                 vector<int> temp = {nums[i],nums[j],nums[a],nums[b]};
-        //                 ans.push_back(temp);
-        //                 a++;
-        //                 b--;
-        //                 while(a<b && nums[a]==nums[a-1]) a++;
-        //                 while(a<b && nums[b]==nums[b+1]) b--;
-        //             }else if(sum > target){
-        //                 b--;
-        //             }else{
-        //                 a++;
-        //             }
-        //         }
-        //     }
-        // }
-        // return ans;
     }
 };
