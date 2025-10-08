df2db3e15c3efca14ba95f9b454368e48f552f95
Yash-C1 <56033766+Yash-C1@users.noreply.github.com>
2025-10-08 16:12:14 -0700

    Time: 3 ms (40.25%), Space: 23.7 MB (13.06%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,76 +1,24 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string ans="";
-        string temp="";
+
+        string ans = "";
+        string temp = "";
+        int space_counter = 0;
         for(int i=0;i<s.size();i++){
-            
-            if(s[i]==' '){
-                if(ans!="" && temp!="") ans = temp+ " "+ans;
-                else ans = temp+ans;
-                temp="";
-            }else{
+            if(s[i]!=' ') {
+                space_counter=0;
                 temp+=s[i];
             }
-
+            if(s[i]==' ' && temp!=""){
+                space_counter++;
+                if(space_counter==1) ans = temp + " " + ans;
+                temp = "";
+            }
+            if(i==s.size()-1 && s[i]!=' ') ans = temp+ " "+ ans;
         }
-        if(ans!="" && temp!="") ans = temp+ " "+ans;
-        else ans = temp+ans;
+        if(ans!="") ans.erase(ans.size()-1);
         return ans;
-
-        // string ans = "";
-        // string temp = "";
-        // for(int i = 0;i<s.size();i++){
-        //     if(s[i]==' '){
-        //         if(ans!="" && temp !="") ans = temp+" "+ans;
-        //         else{
-        //             ans = temp+ans;
-        //         }
-        //         temp="";
-
-        //     }else{
-        //         temp+=s[i];
-        //     }
-        // }
-        // if(ans!="" && temp!="") ans = temp+ " "+ans;
-        // else ans = temp+ans;
-        // return ans;
-
-
-
-
-
-
-
-
-
-
-        // string ans = "";
-        // string temp = "";
-        // int space_counter = 0;
-        // for(int i=0;i<s.size();i++){
-        //     if(s[i]!=' ') {
-        //         space_counter=0;
-        //         temp+=s[i];
-        //     }
-        //     if(s[i]==' ' && temp!=""){
-        //         space_counter++;
-        //         if(space_counter==1) ans = temp + " " + ans;
-        //         // else if(space_counter==1 && ans=="") ans = temp + ans;
-        //         temp = "";
-        //     }
-        //     if(i==s.size()-1 && s[i]!=' ') ans = temp+ " "+ ans;
-        // }
-        // if(ans!="") ans.erase(ans.size()-1);
-        // return ans;
-
-
-
-
-
-
-
-
 
 
     }
