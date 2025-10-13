0d44ff81c6153184ec3c68e8ee8f6589cb6d7f82
Yash-C1 <56033766+Yash-C1@users.noreply.github.com>
2025-10-13 16:19:36 -0700

    Time: 0 ms (100%), Space: 118 MB (71.18%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -11,83 +11,33 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) return true; 
+        ListNode* slow = head;
+        ListNode* fast = head;
         
-        int counter = 0;
-        if(!head || !head->next) return true;
-        ListNode* temp = head;
-        while(temp){
-            counter++;
-            temp = temp->next;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        int mid_point = counter/2;
-        counter=0;
-        ListNode* mover1 = head;
-        ListNode* mover2 = head;
-        while(counter!=mid_point){
-            counter++;
-            mover2 = mover2->next;
-        }
-
+       
         ListNode* prev = NULL;
-        while(mover2){
-            ListNode* temp = mover2->next;
-            mover2->next = prev;
-            prev = mover2;
-            mover2 = temp;
+        while (slow) {
+            ListNode* nextNode = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nextNode;
         }
 
-
-
-        while(prev){
-            if(mover1->val!=prev->val) return false;
-            mover1 = mover1->next;
-            prev = prev->next;
+       
+        ListNode* left = head;  
+        ListNode* right = prev;  
+        while (right) {
+            if (left->val != right->val) return false;
+            left = left->next;
+            right = right->next;
         }
+
         return true;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // if (!head || !head->next) return true; 
-
-      
-        // ListNode* slow = head;
-        // ListNode* fast = head;
-        // while (fast && fast->next) {
-        //     slow = slow->next;
-        //     fast = fast->next->next;
-        // }
-
-       
-        // ListNode* prev = NULL;
-        // while (slow) {
-        //     ListNode* nextNode = slow->next;
-        //     slow->next = prev;
-        //     prev = slow;
-        //     slow = nextNode;
-        // }
-
-       
-        // ListNode* left = head;  
-        // ListNode* right = prev;  
-        // while (right) {
-        //     if (left->val != right->val) return false;
-        //     left = left->next;
-        //     right = right->next;
-        // }
-
-        // return true;
     }
 };
