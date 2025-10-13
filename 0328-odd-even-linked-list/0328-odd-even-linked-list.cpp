fee2a1e54d3a0c12ae60e9e54a20ada2fb93e931
Yash-C1 <56033766+Yash-C1@users.noreply.github.com>
2025-10-13 16:56:20 -0700

    Time: 0 ms (100%), Space: 15.5 MB (88.03%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -24,54 +24,5 @@ public:
 
         odd->next = even_head;
         return head;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // ListNode* odd = head;
-        // ListNode* even = head->next;
-        // ListNode* temp = head;
-        // while(temp!=NULL){
-        //     ListNode* temp2 = temp->next;
-        //     temp->next = temp->next->next;
-        //     temp = temp2->next;
-        // }
-
-        // ListNode* trav = odd;
-        // while(trav->next!=NULL){
-        //     trav = trav->next;
-        // }
-        // trav->next = even;
-
-        // return odd;
-
-        // if (!head || !head->next) return head;  // Handle edge cases
-    
-        // ListNode* odd = head;
-        // ListNode* even = head->next;
-        // ListNode* evenHead = even;  // Store the start of even list
-
-        // while (even && even->next) {  // Ensure even and even->next are not NULL
-        //     odd->next = even->next;
-        //     odd = odd->next;
-        //     even->next = odd->next;
-        //     even = even->next;
-        // }
-
-        // odd->next = evenHead;  // Connect odd and even lists
-
-        // return head;
     }
 };
