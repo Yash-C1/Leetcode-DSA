7d3202a92d1873ca2678700877c395f39848ec03
Yash-C1 <56033766+Yash-C1@users.noreply.github.com>
2025-10-13 16:01:14 -0700

    Time: 5 ms (83.15%), Space: 11.3 MB (52.7%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -10,51 +10,33 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
         // Brute force solution
-        unordered_map<ListNode*,int> mpp;
-        ListNode* temp = head;
-        if(temp==NULL || temp->next==NULL) return NULL;
-        while(temp){
-            if(mpp.find(temp)!=mpp.end()) return temp;
-            else mpp[temp]++;
-            temp = temp->next;
-        }
-        return NULL;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // ListNode* fast = head;
-        // ListNode* slow = head;
-        // while(fast!=NULL && fast->next!=NULL){
-        //     slow = slow->next;
-        //     fast = fast->next->next;
-        //     if(slow==fast) {
-        //         slow = head;
-        //         while(true){
-        //             if(slow==fast) return slow;
-        //             slow = slow->next;
-        //             fast = fast->next;
-                    
-        //         }
-        //     }
+        // unordered_map<ListNode*,int> mpp;
+        // ListNode* temp = head;
+        // if(temp==NULL || temp->next==NULL) return NULL;
+        // while(temp){
+        //     if(mpp.find(temp)!=mpp.end()) return temp;
+        //     else mpp[temp]++;
+        //     temp = temp->next;
         // }
         // return NULL;
+
+        
+        // Optimal solution
+        if(head==NULL || head->next==NULL) return NULL;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast==slow){
+                slow = head;
+                while(fast!=slow){
+                    fast=fast->next;
+                    slow=slow->next;
+                }
+                return fast;
+            }
+        }
+        return NULL;
     }
 };
