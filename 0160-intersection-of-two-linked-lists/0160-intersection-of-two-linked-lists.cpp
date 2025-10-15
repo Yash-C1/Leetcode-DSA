46f14ecc81e00d720e04fb01c8ff695964ef871f
Yash-C1 <56033766+Yash-C1@users.noreply.github.com>
2025-10-14 22:27:09 -0700

    Time: 41 ms (34.83%), Space: 18.4 MB (98.29%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,19 +9,63 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_map<ListNode*,int>mpp;
+        // Brute force method
+        // unordered_map<ListNode*,int>mpp;
+        // ListNode* traverse = headA;
+        // while(traverse){
+        //     mpp[traverse]++;
+        //     traverse = traverse->next;
+        // }
+        // traverse = headB;
+        // while(traverse){
+        //     if(mpp.find(traverse)!=mpp.end()){
+        //         return traverse;
+        //     }
+        //     traverse = traverse->next;
+        // }
+        // return NULL;
+
+
+
+        // Optimal approach
+        int length1 = 0;
+        int length2 = 0;
         ListNode* traverse = headA;
         while(traverse){
-            mpp[traverse]++;
+            length1++;
             traverse = traverse->next;
         }
+
         traverse = headB;
         while(traverse){
-            if(mpp.find(traverse)!=mpp.end()){
-                return traverse;
-            }
+            length2++;
             traverse = traverse->next;
         }
+
+        ListNode* temp = NULL;
+
+        if(length1>length2){
+            traverse = headA;
+            while(length2<length1) {
+                traverse = traverse->next;
+                length2++;
+            }
+            temp = headB;
+        }else{
+            traverse = headB;
+            while(length1<length2) {
+                traverse = traverse->next;
+                length1++;
+            }
+            temp = headA;
+        }
+
+        while(traverse){
+            if(traverse==temp) return traverse;
+            traverse = traverse->next;
+            temp = temp->next;
+        }
+
         return NULL;
     }
 };
