8e2495a8abefbb7fed7dd957f84a525034ac06cd
Yash-C1 <56033766+Yash-C1@users.noreply.github.com>
2025-10-15 16:55:19 -0700

    Time: 0 ms (100%), Space: 8.6 MB (44.2%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
+    void helper(double &x, double &ans, long n){
+        if(n<=0) return;
+        if(n%2==0){
+            x*=x;
+            n/=2;
+        }else{
+            ans*=x;
+            n--;
+        }
+        helper(x,ans,n);
+    }
     double myPow(double x, int n) {
         long abs_n = labs(n);
         double ans = 1;
-        while(abs_n>0){
-            if(abs_n%2==0){
-                x*=x;
-                abs_n/=2;
-            }else{
-                ans*=x;
-                abs_n--;
-            }
-        }
+        helper(x,ans,abs_n);
+
+        // while(abs_n>0){
+        //     if(abs_n%2==0){
+        //         x*=x;
+        //         abs_n/=2;
+        //     }else{
+        //         ans*=x;
+        //         abs_n--;
+        //     }
+        // }
         
         ans = (n>0) ? ans : (1/ans);
         return ans;
