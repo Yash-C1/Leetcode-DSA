a9ce4c96fe80f6b6c6e5e6b6a8eb2d24615c3f8e
Yash-C1 <56033766+Yash-C1@users.noreply.github.com>
2025-12-30 23:33:42 -0800

    Time: 131 ms (11.19%), Space: 144.8 MB (10.2%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -10,37 +10,50 @@ public:
         return true;
     }
 
-    void solve(string s, vector<string> op, vector<vector<string>>& ans){
-        if(s.empty()){
-            for(auto &str : op)
-                if(!check_palindrome(str)) return;
+    // void solve(string s, vector<string> op, vector<vector<string>>& ans){
+    //     if(s.empty()){
+    //         for(auto &str : op)
+    //             if(!check_palindrome(str)) return;
+    //         ans.push_back(op);
+    //         return;
+    //     }
+
+    //     string s1 = s;
+    //     string last = op.back();
+    //     last.push_back(s[0]);
+    //     op.pop_back();
+    //     op.push_back(last);
+    //     s1.erase(s1.begin());
+    //     solve(s1, op, ans); // extend last substring
+
+    //     // backtrack and start new substring
+    //     last.pop_back();
+    //     op.pop_back();
+    //     op.push_back(last);
+    //     op.push_back(string(1, s[0]));
+    //     solve(s1, op, ans); // new substring
+    // }
+
+    void solve(string s, vector<string> op, vector<vector<string>>& ans, int index){
+        if(index==s.length()){
             ans.push_back(op);
             return;
         }
 
-        string s1 = s;
-        string last = op.back();
-        last.push_back(s[0]);
-        op.pop_back();
-        op.push_back(last);
-        s1.erase(s1.begin());
-        solve(s1, op, ans); // extend last substring
-
-        // backtrack and start new substring
-        last.pop_back();
-        op.pop_back();
-        op.push_back(last);
-        op.push_back(string(1, s[0]));
-        solve(s1, op, ans); // new substring
+        for(int i=index;i<s.size();i++){
+            if(check_palindrome(s.substr(index,i - index + 1))){
+                op.push_back(s.substr(index,i - index + 1));
+                solve(s, op, ans, i+1);
+                op.pop_back();
+            }
+        }
     }
 
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
         if(s.empty()) return ans;
         vector<string> op;
-        op.push_back(string(1, s[0]));
-        s.erase(s.begin());
-        solve(s, op, ans);
+        solve(s, op, ans, 0);
         return ans;
     }
 };
