3118a07cfd823d7fe2ea7aadb76360601d789487
Yash-C1 <56033766+Yash-C1@users.noreply.github.com>
2025-12-31 19:02:10 -0800

    Time: 47 ms (52.9%), Space: 57.6 MB (51.05%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,7 +1,8 @@
 class Solution {
-public:
+private:
     bool check_palindrome(string s){
-        int i = 0, j = s.size()-1;
+        int i=0;
+        int j = s.length()-1;
         while(i<j){
             if(s[i]!=s[j]) return false;
             i++;
@@ -9,53 +10,26 @@ public:
         }
         return true;
     }
-
-    // void solve(string s, vector<string> op, vector<vector<string>>& ans){
-    //     if(s.empty()){
-    //         for(auto &str : op)
-    //             if(!check_palindrome(str)) return;
-    //         ans.push_back(op);
-    //         return;
-    //     }
-
-    //     string s1 = s;
-    //     string last = op.back();
-    //     last.push_back(s[0]);
-    //     op.pop_back();
-    //     op.push_back(last);
-    //     s1.erase(s1.begin());
-    //     solve(s1, op, ans); // extend last substring
-
-    //     // backtrack and start new substring
-    //     last.pop_back();
-    //     op.pop_back();
-    //     op.push_back(last);
-    //     op.push_back(string(1, s[0]));
-    //     solve(s1, op, ans); // new substring
-    // }
-
-    void solve(string s, vector<string> op, vector<vector<string>>& ans, int index){
-        if(index==s.length()){
-            ans.push_back(op);
+    void helper(vector<vector<string>> &ans, vector<string>&ds, string s, int index){
+        if(index==s.size()){
+            ans.push_back(ds);
             return;
         }
 
-        for(int i=index;i<s.size();i++){
-            if(check_palindrome(s.substr(index,i - index + 1))){
-                op.push_back(s.substr(index,i - index + 1));
-                solve(s, op, ans, i+1);
-                op.pop_back();
+        for(int i=index;i<s.length();i++){
+            string temp = s.substr(index,i-index+1);
+            if(check_palindrome(temp)){
+                ds.push_back(temp);
+                helper(ans,ds,s,i+1);
+                ds.pop_back();
             }
         }
     }
-
+public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
-        if(s.empty()) return ans;
-        vector<string> op;
-        solve(s, op, ans, 0);
+        vector<string>ds;
+        helper(ans,ds,s,0);
         return ans;
     }
 };
-
-
