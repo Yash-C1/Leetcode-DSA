893144ac14cdb84e596e45b39b1548aa2b2dae31
Yash-C1 <56033766+Yash-C1@users.noreply.github.com>
2026-01-03 16:48:59 -0800

    Time: 0 ms (100%), Space: 23.4 MB (47.95%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,22 +1,64 @@
+// class MinStack {
+// private:
+//     stack<pair<int,int>> st;
+//     int size;
+// public:
+//     MinStack() {
+//         size = 0;
+//     }
+    
+//     void push(int val) {
+//         if(st.empty()) {
+//             st.push( {val, val} );
+//             return;
+//         }
+//         st.push({val,min(getMin(),val)});
+//     }
+    
+//     void pop() {
+//         st.pop();
+//     }
+    
+//     int top() {
+//         return st.top().first;
+//     }
+    
+//     int getMin() {
+//         return st.top().second;
+//     }
+// };
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * MinStack* obj = new MinStack();
+//  * obj->push(val);
+//  * obj->pop();
+//  * int param_3 = obj->top();
+//  * int param_4 = obj->getMin();
+//  */
+
+
+
 class MinStack {
 private:
     stack<pair<int,int>> st;
+    int min_;
     int size;
 public:
     MinStack() {
+        min_ = INT_MAX;
         size = 0;
     }
     
     void push(int val) {
-        if(st.empty()) {
-            st.push( {val, val} );
-            return;
-        }
-        st.push({val,min(getMin(),val)});
+        min_ = min(min_,val);
+        st.push({val,min_});
     }
     
     void pop() {
         st.pop();
+        if(st.empty()) min_ = INT_MAX;
+        else min_ = st.top().second;
     }
     
     int top() {
