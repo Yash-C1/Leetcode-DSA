d6356506f097b5e53b567698cfbe46b0e968742a
Yash-C1 <56033766+Yash-C1@users.noreply.github.com>
2026-01-09 16:56:59 -0800

    Time: 3 ms (18.07%), Space: 27.3 MB (17.88%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,61 +1,27 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int ans = 0;
-        int width = 0;
-        int start = 0;
-        int cnt = 0;
-        int reverse_cnt = 0;
-        stack<int> st;
+        int ans=0;
         int n = height.size();
-
-        for(int i=0;i<height.size();i++){
-            while(!st.empty() && st.top()<=height[i]){
-                st.pop();
-            }
-
-            if(st.empty()){
-                int volume = (width * start) - cnt;
-                ans += volume;
-                start = height[i];
-                cnt = 0;
-                width = 0;
-            }else{
-                width++;
-                cnt += height[i];
-            }
-
-            st.push(height[i]);
+        vector<int> left(n,0);
+        vector<int> right(n,0);
+        int prev = 0;
+        for(int i=0;i<n;i++){
+            left[i] = max(height[i],prev);
+            prev = left[i];
         }
 
-        if(st.size()==1){
-            return ans;
+        prev = 0;
+        for(int i=n-1;i>=0;i--){
+            right[i] = max(height[i],prev);
+            prev = right[i];
         }
 
-        while(!st.empty()) st.pop();
-        int width2 = 0;
-        int start2 = 0;
-        cnt = 0;
-
-        for(int i=0;i<=width;i++){
-            while(!st.empty() && st.top()<=height[n-i-1]){
-                st.pop();
-            }
-
-            if(st.empty()){
-                int volume = (width2 * start2) - cnt;
-                ans += volume;
-                start2 = height[n-i-1];
-                cnt = 0;
-                width2 = 0;
-            }else{
-                width2++;
-                cnt += height[n-i-1];
-            }
-
-            st.push(height[n-i-1]);
-
+        for(int i=0;i<n;i++){
+            int vol = min(right[i],left[i]) - height[i];
+            ans+=vol;
         }
         return ans;
+
     }
 };
